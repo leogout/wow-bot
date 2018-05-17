1fbfa413724b022aca088962b1343f4564f8e488
Léo Richard <leogout@gmail.com>
2018-05-17 16:43:44 +0200

    Removed a lot of unused functions on screen detection

--- screen.py
+++ screen.py
@@ -32,11 +32,7 @@ def get_data():
     return {
         'spells': spells(screen),
         'target': target_type(screen),
-        'hhp': hero_hp(screen),
-        'ehp': enemy_hp(screen),
-        'ecast': enemy_cast(screen),
         'holy': holy_power_count(screen),
-        'oor': incorrect_position(screen)
     }
 
 
@@ -61,44 +57,9 @@ def spells(img):
 
 def target_type(img):
     r, g, b, *a = img.getpixel((252, 35))
-    print(r, g, b)
-    if eq((r, g, b), (0, 0, 190), bias=10):
-        return 'ally'
-    if eq((r, g, b), (0, 190, 0), bias=10):
-        return 'pnj'
-    if eq((r, g, b), (102, 101, 102), bias=10) or eq((r, g, b), (203, 200, 0), bias=10) or eq((r, g, b), (190, 190, 0), bias=10):
-        return 'neutral'
+
     if eq((r, g, b), (190, 0, 0), bias=10):
         return 'enemy'
-    return 'none'
-
-
-def hero_hp(img):
-    green = 0
-    for x in range(0, HHP_LENGTH):
-        r, g, b, *a = img.getpixel((HHP_X + x, HHP_Y))
-
-        if r < 10 and g > 90 and b < 10:
-            green += 1
-
-    return green / HHP_LENGTH * 100
-
-
-def enemy_hp(img):
-    green = 0
-    for x in range(0, EHP_LENGTH):
-        r, g, b, *a = img.getpixel((EHP_X + x, EHP_Y))
-
-        if r < 10 and g > 90 and b < 10:
-            green += 1
-
-    return green / EHP_LENGTH * 100
-
-
-def enemy_cast(img):
-    r, g, b, *a = img.getpixel((ECAST_X, ECAST_Y))
-
-    return eq((r, g, b), (174, 121, 0), bias=5) or is_green(r, g, b)
 
 
 def holy_power_count(img):
@@ -109,20 +70,3 @@ def holy_power_count(img):
             hp_count += 1
 
     return hp_count
-
-
-def incorrect_position(img):
-    def detect_e(x):
-        r1, g1, b1, *a1 = img.getpixel((x, 130))
-        r2, g2, b2, *a2 = img.getpixel((x, 131))
-        r3, g3, b3, *a3 = img.getpixel((x, 134))
-        r4, g4, b4, *a4 = img.getpixel((x, 135))
-        return is_red(r1, g1, b1) and is_black(r2, g2, b2) and is_red(r3, g3, b3) and is_black(r4, g4, b4)
-
-    out_of_range = detect_e(956) and detect_e(1005) and detect_e(1015)
-    cant_attack = detect_e(856) and detect_e(913) and detect_e(1021)
-    wrong_direction = detect_e(840) and detect_e(881) and detect_e(961)
-    target_not_in_front = detect_e(882) and detect_e(941) and detect_e(972)
-
-    return out_of_range or cant_attack or wrong_direction or target_not_in_front
-
